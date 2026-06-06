b2f27628cc2a61641af8e7b4d89fe25cfae00ae4
samnayana@gmail.com <samnayana@gmail.com>
2026-06-06 14:14:58 +0530

    use a simpler approach

--- portals/apps/trader-app/vite.config.ts
+++ portals/apps/trader-app/vite.config.ts
@@ -5,9 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: process.env.HOST || 'localhost', // Exposes Vite on the local network if HOST is set
-    port: 5173,
-    strictPort: true, // Prevents Vite from auto-switching ports if 5173 is busy
-  }
 })
