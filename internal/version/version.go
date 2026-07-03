73d59821d30f99aac6489b8502ec0e7b5f4a61d8
saknarajapakshe <rajapaksherars.22@uom.lk>
2026-07-03 09:32:10 +0530

    fix(version): update version variable to be unexported and use getter method

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,8 +1,13 @@
 package version
 
-// Version is set at build time via -ldflags:
+// version is set at build time via -ldflags:
 //
-//	go build -ldflags="-X github.com/OpenNSW/nsw-srilanka/internal/version.Version=1.2.3"
+//	go build -ldflags="-X github.com/OpenNSW/nsw-srilanka/internal/version.version=1.2.3"
 //
 // Defaults to "dev" for local builds.
-var Version = "dev"
+var version = "dev"
+
+// Get returns the embedded application build version.
+func Get() string {
+	return version
+}
